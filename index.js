21974333e7e7a9bb6dfcedb811ff5b62fdb32741
victor <victor.buthia@student.moringaschool.com>
2022-11-16 14:25:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+  }
+  return messages;
+
+}
+console.log(writeCards(['Guadalupe', ' Ollie', 'Aki'], 'birthday'));
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
